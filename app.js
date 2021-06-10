6004548d5759f1edda42b76f3dd72fbf02935743
Frikkie-hub <85674327+Frikkie-hub@users.noreply.github.com>
2021-06-10 11:10:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-git add app.js
-git commit -m "change message"
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello GFR!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+git push
